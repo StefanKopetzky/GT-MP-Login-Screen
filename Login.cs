39167a7b3acf69bc76c8c89ae2da52e0347615d7
Neta <admin@nsys.pw>
2017-09-24 11:12:32 +0200

    Initial commit

---
+++ Login.cs
@@ -0,0 +1,15 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+
+namespace GameLoginScreen
+{
+    public class Login : Script
+    {
+		[Command("openlogin")]
+		public void OpenLogin(Client sender)
+		{
+			API.triggerClientEvent(sender, "setLoginUiVisible", true);
+		}
+	}	
+}
